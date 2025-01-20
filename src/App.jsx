7865908d6737ade3bfb89f29a3cec2edd2581d9b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-01-20 23:19:36 +0330

    building the pages using router

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import HomePages from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
         <Route path="/" element={<HomePages />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 const PageNav = () => {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -14,6 +17,11 @@ const PageNav = () => {
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
