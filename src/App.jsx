522d57dc452dbcc8360a5897183eeecad9baa12f
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-01-23 21:25:14 +0330

    fixing redirection to cities pages by default when visiting app page by Navigate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -44,10 +44,7 @@ const App = () => {
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
