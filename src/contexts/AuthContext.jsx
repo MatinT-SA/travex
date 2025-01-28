dc13f34e8bc35ae8c6e35bf33960b461bf5de4d7
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-01-28 22:43:28 +0330

    implementing another context for our authentication

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useReducer, useContext } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const USER_TEST = {
+  name: "Matin",
+  email: "Matin@example.com",
+  password: "wimbledon",
+  avatar: "https://i.pravatar.cc/100?u=ak",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error("Invalid action type");
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === USER_TEST.email && password === USER_TEST.password) {
+      dispatch({ type: "login", payload: USER_TEST });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext must be used within an AuthProvider");
+}
+
+export { AuthProvider, useAuth };
