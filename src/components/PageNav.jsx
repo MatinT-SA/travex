a99b0394ae98f3f0862cb3cf893711e419b587da
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-01 17:58:32 +0330

    fixing hamburger icon

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Hamburger from "hamburger-react";
-import styles from "./PageNav.module.css";
+import styles from "./PageNav.module.css"; // Ensure correct path
 import Logo from "./Logo";
 
 const PageNav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen((prevState) => !prevState);
-  };
-
   return (
     <nav className={styles.nav}>
       <Logo />
 
+      {/* Hamburger Icon */}
       <div className={styles.hamburgerIcon}>
-        <Hamburger toggled={isOpen} toggle={toggleMenu} />
+        <Hamburger toggled={isOpen} toggle={setIsOpen} />
       </div>
 
+      {/* Sliding Navigation Menu */}
       <ul className={`${styles.navList} ${isOpen ? styles.open : ""}`}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/pricing" onClick={() => setIsOpen(false)}>
+            Pricing
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/product" onClick={() => setIsOpen(false)}>
+            Product
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/login" className={styles.ctaLink}>
+          <NavLink
+            to="/login"
+            className={styles.ctaLink}
+            onClick={() => setIsOpen(false)}
+          >
             Login
           </NavLink>
         </li>
