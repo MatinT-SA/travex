d0d3cfae08c653eee237a1d81d0b5e2471c0e5d0
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-03 19:52:27 +0330

    completing responsiveness

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
-import User from "../components/User";
 
 const AppLayout = () => {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
-      <User />
     </div>
   );
 };
