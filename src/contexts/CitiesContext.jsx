0f8860ef3d5fb6d58cda71d78adf32b22b6a3531
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-02-07 21:10:15 +0330

    console log inside fetchCities for test

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -73,6 +73,7 @@ function CitiesProvider({ children }) {
       try {
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
+        console.log(cities);
         dispatch({ type: "cities/loaded", payload: data });
       } catch (error) {
         dispatch({
